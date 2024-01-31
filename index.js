56feb7133f50e8816f3473d84d72b9c3c6048a8d
ArtemBasharin <temon-bash@yandex.ru>
2024-01-31 10:09:52 +0300

    add

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// window.addEventListener("resize", () => {
-//   let vh = window.innerHeight / 100;
-//   let vw = window.innerWidth / 100;
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight / 100;
+  let vw = window.innerWidth / 100;
 
-//   document.documentElement.style.setProperty("--vh", `${vh}px`);
-//   document.documentElement.style.setProperty("--vw", `${vw}px`);
-// });
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
+});
 
 function hideAddressBar() {
   if (!window.location.hash) {
