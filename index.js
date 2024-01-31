5a365a8dda200eff9ba75d61cd7b3137453977fa
ArtemBasharin <temon-bash@yandex.ru>
2024-01-31 07:00:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight / 100;
-  let vw = window.innerWidth / 100;
+// window.addEventListener("resize", () => {
+//   let vh = window.innerHeight / 100;
+//   let vw = window.innerWidth / 100;
 
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-  document.documentElement.style.setProperty("--vw", `${vw}px`);
+//   document.documentElement.style.setProperty("--vh", `${vh}px`);
+//   document.documentElement.style.setProperty("--vw", `${vw}px`);
+// });
+
+function hideAddressBar() {
+  if (!window.location.hash) {
+    if (document.height < window.outerHeight) {
+      document.body.style.height = window.outerHeight + 50 + "px";
+    }
+
+    setTimeout(function () {
+      window.scrollTo(0, 1);
+    }, 50);
+  }
+}
+
+window.addEventListener("load", function () {
+  if (!window.scrollY) {
+    hideAddressBar();
+  }
 });
+window.addEventListener("orientationchange", hideAddressBar);
