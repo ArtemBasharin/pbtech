8715382b44cf54abde9551f3d84ea7526ae08abe
ArtemBasharin <temon-bash@yandex.ru>
2024-02-21 06:20:40 +0300

    add

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// import * as THREE from "./three";
+// import TweenMax from "gsap";
+// import THREE from "./node_modules/three/src/Three";
+// import TweenMax from "./node_modules/gsap/gsap-core";
+import Kute from "./node_modules/kute.js";
+response.setHeader("Content-Type", "application/javascript");
+
 window.addEventListener("resize", () => {
   let vh = window.innerHeight / 100;
   let vw = window.innerWidth / 100;
@@ -38,3 +45,24 @@ for (let i = 1; i < 5; i++) {
   btn.style.setProperty(`--random-delay${i}`, `${Math.random() * 12}s`);
 }
 
+// Создание SVG изображения
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("width", "100");
+svg.setAttribute("height", "100");
+svg.innerHTML = "<rect x='10' y='10' width='30' height='30' fill='red' />";
+document.body.appendChild(svg);
+// Создание анимации
+var animation = new Kute.Animation();
+// Добавление файлов картинок
+animation.add(svg, {
+  duration: 1,
+  easing: "easeInOut",
+  animate: function (value) {
+    svg.innerHTML = "<image xlink:href='image1.png' width='30' height='30' />";
+  },
+  animate: function (value) {
+    svg.innerHTML = "<image xlink:href='image2.png' width='30' height='30' />";
+  }
+});
+// Запуск анимации
+animation.play();
