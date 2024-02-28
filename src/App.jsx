ea43c005c88424d7c0ce089ec40cc2418c58cdee
ArtemBasharin <temon-bash@yandex.ru>
2024-02-28 06:54:49 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "'./images/pblogo.png'";
+import logo from "../images/pblogo.png";
 
 function App() {
   window.addEventListener("resize", () => {
